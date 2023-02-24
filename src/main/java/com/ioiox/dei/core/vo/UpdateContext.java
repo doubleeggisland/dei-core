ef0818cbd5b5f2bc79453d902c8c15d22e1080c0
chengshan <shanc@133.cn>
2023-02-24 10:42:52 +0800

    实现标准数据接口服务的基础模型类

--- src/main/java/com/ioiox/dei/core/vo/UpdateContext.java
+++ src/main/java/com/ioiox/dei/core/vo/UpdateContext.java
@@ -3,4 +3,20 @@ package com.ioiox.dei.core.vo;
 public abstract class UpdateContext<T extends UpdatableVO> {
     private boolean needUpdate;
     private T updatableObj;
+
+    public boolean isNeedUpdate() {
+        return needUpdate;
+    }
+
+    public void setNeedUpdate(boolean needUpdate) {
+        this.needUpdate = needUpdate;
+    }
+
+    public T getUpdatableObj() {
+        return updatableObj;
+    }
+
+    public void setUpdatableObj(T updatableObj) {
+        this.updatableObj = updatableObj;
+    }
 }
